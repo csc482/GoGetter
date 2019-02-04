fa0ea13c302feffe8162c60ca630dda58d799201
coldmustard-alec <abg523@gmail.com>
2019-02-04 18:36:56 -0500

    gitignore and http update

--- getter.go
+++ getter.go
@@ -4,6 +4,26 @@
    >> report results (or errors) to Loggly.
 */
 
-//LET'S GET THIS BREAD
+///////////////////////////////[LET'S GET THIS BREAD]/////////////////////////////////
 
+package main
 
+import "net/http"
+import "fmt"
+
+func main(){
+
+  resp, err := http.Get("https://api.darksky.net/forecast/1f960db8bf1129b90c3ee6e265c92924/47.8267,-122.4233")
+
+  if err != nil {
+
+    fmt.Println("%s", resp)
+    fmt.Println("hi")
+
+  } else {
+    
+    fmt.Println("bye")
+
+  }
+
+}
