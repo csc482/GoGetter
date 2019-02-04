ec4ccb9f7827dea56b4a2d2985c72d7e1cfc54da
coldmustard-alec <abg523@gmail.com>
2019-02-04 17:23:03 -0500

    initialize Go program

---
+++ getter.go
@@ -0,0 +1,9 @@
+/* TODO: 
+   >> periodically collect formatted data via API requests 
+   >> display this data on the console
+   >> report results (or errors) to Loggly.
+*/
+
+//LET'S GET THIS BREAD
+
+
